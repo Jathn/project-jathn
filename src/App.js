2ebe2d98606b73efb52c8b3b32330cc56026dcd5
Jathn <jonathan.ramstedt@gmail.com>
2024-01-31 15:20:04 +0200

    Updated to technology experience view instead of ugly text version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Header from './components/Header';
-import SingleFile from './components/SingleFile';
 import DoubleFile from './components/DoubleFile';
 import TableView from './components/TableView';
 
@@ -44,28 +43,28 @@ function App() {
                       /* 
                       Project 1
                       **/
-                      ["Car Dealership Website", 
+                      ["Car Dealership Website","",
                       "Handles car dealers and their cars from databases. User authentication system for review posting.", 
                       "Django for main back and front end, Cloud NLP AI API, Flask and Node for local servers", 
                       "https://github.com/Jathn/agfzb-CloudAppDevelopment_Capstone"],
                       /*
                       Project 2 
                       **/
-                      ["Auto Price Estimator", 
+                      ["Auto Price Estimator","",
                       "Makes a prediction of a cars price based on inputs specified in the little API that was also created.", 
                       "Pandas, MatPlotLib, Gradio, scikit-learn", 
                       "https://github.com/Jathn/auto-price-estimator"],
                       /*
                       Project 3 
                       **/
-                      ["Fake News Classification",
+                      ["Fake News Classification","",
                       "A detailed Project where the truth value of news was classified with ML. Handled a dataset with 40,000 instances to get a satisfactory result. ",
                       "pandas, MatPlotLib, scikit-learn, NLP with nltk library.",
                       "https://github.com/Jathn/fakenews_detector/"],
                       /*
                       Project 4
                       **/
-                      ["Jammming",
+                      ["Jammming","",
                       "A front-end application for playlist creation. Handles requests to the spotify API.", 
                       "React", 
                       "https://github.com/Jathn/Jammming"],
@@ -73,22 +72,25 @@ function App() {
   /* 
   Work experience
   **/
-  const title2 = "Experience";
-  const underTitle2 = "Work, other projects and programming languages."
-  const text2 = "My language of choice for ML so far has been python, because of things like jupyter notebook and well implemented libraries. " +
-                "For web development I am familiar with both JS and python, using different frameworks such as those in the projects above. " +
-                "Apart from these, I also have familiarity with scala, C and C++. The knowledge of these have come from my studies at Aalto University. " +
-                "In C and C++ I now grasp the fundamentals of software development. In C I have mostly implemented simple programs, such as CLI projects. " +
-                "C++, I have also used to create arduino projects, the largest so far being a whack a mole game using a 3D printed box with large led push buttons. "  +
-                "Apart from this knowledge I have also gained a good understanding of workplaces by working summers in the restaurant business since I was 15."
+  const programming = [
+    ["JavaScript", "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png", "Experience level: Intermediate", "Familiarity with Node, Express and React. Knowledge of creating tested & secure full-stack web apps."],
+    ["Python", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXT2V0192ugY3c5ocZ5noZvxxwbfEOXZ8I2g&usqp=CAU", "Experience level: Advanced", "Machine learning, web development, and data science. Familiarity with many libraries and frameworks and adapting to new ones is no issue."],
+    ["Scala", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSupUwOPBgxqtKzv0MLtkJTQ4eY-VeHto27Vg&usqp=CAU", "Experience level: Intermediate", "CS concepts: Algorithmic programming, data structures, and functional programming."],
+    ["C++", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqysu4hCQdVx47zRTnherSUX0MRL0VtzoHLg&usqp=CAU", "Experience level: Intermediate", "Knowledgeable in C++ for micro controller (Arduino) and game development.", ""],
+    ["IBM cloud", "https://lh4.googleusercontent.com/TvOcd33BzpR7U2tsEwHHutwYxP1vbga3MPzhw_GB6yfMPDBkRq4DnO9KIGEf2bkhv5pkwkdAfw5v_gVSr2tGYzqFOm_DaajQWCN68i-wnNz_x5ZrvStdv-gb7rIXa9mBeauiRczX", "Experience level: Proficient", "Deployment of web applications, cloud functions, and cloud databases."],
+    ["AWS", "https://i.pinimg.com/736x/a7/c6/ed/a7c6ede40689fe04bb72e221945f1a12.jpg", "Limited experience", "My usage with AWS has been around making serverless endpoints utilizing lambda and RDS"],
+    ["NoSQL/SQL", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWqYx0Aw9Hnk-Q4C-B7lUm3n9l2PfLoGqcdw&usqp=CAU", "Experience level: Proficient", "Experience with SQL & NoSQL for database creation, handling and integration. Comfortable with PostgreSQL, sqlite & MongoDB."],
+    ["R", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/R_logo.svg/1086px-R_logo.svg.png", "Experience level: Proficient", "Time Series Analysis & Data visualization."],
+  ];
+
   return (
     <div className="App">
       <Header title={title} message={welcome_msg} />
       <DoubleFile img={image1} title={title1} text={text1} isFirst={true} />
       <DoubleFile title={fsTitle} text={fsText} img_pos="left" img={fsImg} />
       <DoubleFile title={mlTitle} text={mlText} img={mlImg} />
-      <TableView title={projTitle} content={projects} />
-      <SingleFile title={title2} underTitle={underTitle2} text={text2} />
+      <TableView title={projTitle} content={projects} underTitle={"Project, description & their github links:"} secondTitle={"Technologies used:"}/>
+      <TableView title="Technology experience" content={programming} underTitle="" secondTitle={"Usage"}/>
     </div>
   );
 }
--- src/components/TableView.js
+++ src/components/TableView.js
@@ -3,22 +3,26 @@ import './TableView.css';
 
 const TableView = (props) => {
     const title = props.title;
+    const underTitle = props.underTitle;
     const content = props.content;
+    const secondTitle = props.secondTitle;
+
     let content_list_elems = [];
     for (let i = 0; i < content.length; i++) {
 
         let name = content[i][0];
-        let description = content[i][1];
-        let technologies = content[i][2];
-        let link = content[i][3];
+        let img = content[i][1];
+        let description = content[i][2];
+        let technologies = content[i][3];
+        let link = content[i][4];
 
-        content_list_elems.push(<li className="telement"><h3>{name}</h3><p>{description}</p><p>Technologies used: {technologies}</p><a href={link}>{link ? "Link" : "" }</a></li>);
+        content_list_elems.push(<li className="telement"><h3>{name}</h3>{img ? <img src={img} alt={"img"}></img> : ""}<p>{description}</p><p>{secondTitle}</p><p>{technologies}</p><a href={link}>{link ? "Link" : "" }</a></li>);
     };
     return(
         <div className="tableTop">
             <div className="elementList">
                 <h1>{title}</h1>
-                <h2>Project, description & their github links:</h2>
+                <h2>{underTitle}</h2>
                 <ul>
                     {content_list_elems}
                 </ul>
